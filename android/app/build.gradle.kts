1e7e7a80d3da9b915cd3e6358588dd5b62ae7329
SkyDevF <thitiwut.pr@rmuti.ac.th>
2025-07-14 23:45:45 +0700

    แก้ไขสำเร็จ

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -34,8 +34,8 @@ android {
         applicationId = "com.kinroo.app"
         minSdk = 23
         targetSdk = flutter.targetSdkVersion
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.2"
     }
 
     signingConfigs {
